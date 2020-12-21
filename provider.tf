e10f079d7f06aceedda847bf47816aea92e54d34
cbzadmin <cbzadmin@cbzadmins-MacBook-Pro.local>
2020-12-21 16:56:18 +0530

    aws ec2 provision using terraform

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIA5N2XLDRWQ4PPHYH3"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region     = "us-east-2"
 
 }
